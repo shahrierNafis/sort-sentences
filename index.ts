a714b52d0c55fb91d1c0fb46fb97008a0d7ca43d
Shahrier Nafis <102902752+shahrierNafis@users.noreply.github.com>
2024-07-11 06:10:20 +0600

    2nd commit

--- index.ts
+++ index.ts
@@ -12,3 +12,19 @@ function getSentences(text: string, locale = "en") {
 
   return segmenter.segment(text);
 }
+console.log(
+  Array.from(
+    new Intl.Segmenter("en", { granularity: "sentence" }).segment(
+      Array.from(document.querySelectorAll("p"))
+        .map((p) => p.innerText)
+        .join(" ")
+        .replace(/(\r\n|\n|\r)/gm, "")
+        .replace(/\[\d+\]/gm, "")
+        .replace(/\n/g, " ")
+    )
+  )
+    .sort((a, b) => b.segment.length - a.segment.length)
+    .slice(0, 5)
+    .map((x) => x.segment)
+    .join("\n\n")
+);
